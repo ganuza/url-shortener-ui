ffe05b4c07089898a9131873b1853d28ddd05017
Adrian Zabolitzki <adrianzab@gmail.com>
2023-09-29 14:46:36 -0600

    Add thourough cypress testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,8 +3,9 @@ describe('url shortener on page load', () => {
     cy.intercept('GET', 'http://localhost:3001/api/v1/urls', {
       statusCode: 200,
       fixture: 'data'
-    })
+    }).as('getShortUrl')
     cy.visit("http://localhost:3000")
+    cy.wait('@getShortUrl')
   })
 
   it('should have a title, 2 input fields, and a button', () => {
@@ -14,23 +15,39 @@ describe('url shortener on page load', () => {
     cy.get('button').should('exist')
   })
   
-  it('should show existing shortened url', () => {
+  it('should show existing title, long url, and shortened url', () => {
     cy.get('section')
     .children()
-    .get(':nth-child(1)')
-    .contains('http://localhost')
+    .get(':nth-child(1)').contains('Awesome photo')
+    cy.get(':nth-child(1) > a').contains('http://localhost:3001/useshorturl/1')
+    cy.get(':nth-child(1) > p').contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
   })
 
   it('should allow user to type into inputs', () => {
     cy.get('[placeholder="Title..."]').type('test2').should('have.value', 'test2')
+    cy.get('[placeholder="URL to Shorten..."]').type('test description').should('have.value', 'test description')
   })
 
+describe('Tests for POST', () => {
+
   it('should allow user to fill out form', () => {
-    cy.get('[placeholder="Title..."]').type('testTitle')
-    cy.get('[placeholder="URL to Shorten..."]').type('testDescription')
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls',
+    {
+      statusCode:201,
+      body: {"long_url": "https://images.unsplash.com/photo-1695790952932-386b9d66c18a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      "title": "Amazing Tree",
+      "id": 28,
+      "short_url": "http://localhost:3001/useshorturl/28"}
+    })
+    cy.get('[placeholder="Title..."]').type('Amazing Tree')
+    cy.get('[placeholder="URL to Shorten..."]').type('https://images.unsplash.com/photo-1695790952932-386b9d66c18a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60')
     cy.get('button').first().click()
-    cy.get('section > :nth-child(3)').contains('a', 'http://localhost:3001')
+    cy.get('section').children().should('have.length', 3)
+    cy.get(':nth-child(3) > h3').contains('Amazing Tree')
+    cy.get(':nth-child(3) > a').contains('http://localhost:3001/useshorturl/28')
+    cy.get(':nth-child(3) > p').contains('https://images.unsplash.com/photo-1695790952932-386b9d66c18a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60')
   })
+})
 
 
 })
